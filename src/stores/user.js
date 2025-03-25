b58cee6b8e66d29af7c0e28fe41ceb70b5a68bd7
RedBoxing <redboxing@redboxing.moe>
2025-03-25 10:33:21 +0100

    Add: Authentication

---
+++ src/stores/user.js
@@ -0,0 +1,62 @@
+import { ref, computed, inject } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useUserStore = defineStore('user', () => {
+  const $cookies = inject('$cookies')
+  const current = ref(null)
+  const token = ref($cookies.get('jwt_token'))
+  const connected = computed(() => token.value != null)
+
+  if (token.value != undefined && token.value != '') {
+    refreshUser()
+  }
+
+  async function login(username, password) {
+    token.value = (
+      await axios.post(import.meta.env.VITE_BACKEND_URL + '/Auth/login', {
+        login: username,
+        password,
+      })
+    ).data.token
+    $cookies.set('jwt_token', token.value)
+    await refreshUser()
+  }
+
+  async function register(username, password, email, first_name, last_name) {
+    current.value = (
+      await axios.post(import.meta.env.VITE_BACKEND_URL + '/Auth/register', {
+        login: username,
+        password,
+        email,
+        firstName: first_name,
+        lastName: last_name,
+      })
+    ).data.token
+    await refreshUser()
+  }
+
+  async function logout() {
+    token.value = null
+    current.value = null
+    $cookies.set('jwt_token', undefined)
+  }
+
+  async function refreshUser() {
+    try {
+      current.value = (
+        await axios.get(import.meta.env.VITE_BACKEND_URL + '/User/me', {
+          headers: {
+            Authorization: `Bearer ${token.value}`,
+          },
+        })
+      ).data
+    } catch (err) {
+      token.value = null
+      $cookies.set('jwt_token', undefined)
+      throw err
+    }
+  }
+
+  return { current, token, connected, login, register, logout }
+})
