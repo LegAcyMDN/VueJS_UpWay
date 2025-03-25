127fbee36300c0947d080c3e5b20aa21c50d8d1f
AlexisBONNARD <alexis.bonnard@etu.univ-smb.fr>
2025-03-25 17:08:47 +0000

    Add :  AccessoireView

---
+++ src/views/ArticleView.vue
@@ -0,0 +1,17 @@
+<script setup>
+//import HelloWorld from '../components/HelloWorld.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { useArticleStore } from '@/stores/articles';
+import ArticleComponent from '@/components/ArticleComponent.vue';
+const articles = useArticleStore()
+</script>
+
+<template>
+  <main>
+    <p>Article</p>
+    <ArticleComponent v-for="article in articles.listarticle":article="article"/>
+  </main>
+</template>
+
+<style>
+</style>
