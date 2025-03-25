5425c3fc8db523bd9f06c47cfd3a9deb2ae53ad9
AlexisBONNARD <alexis.bonnard@etu.univ-smb.fr>
2025-03-25 09:46:21 +0000

    remouve : icon add: collor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+//import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h2>Vélos électriques Upway : plus de 150 marques et 1600 VAE</h2>
+    <p>Découvrez tous nos modèles de vélos électriques, reconditionnés par une équipe de professionnels, accessibles à tous et garantis un an.</p>
+    <div class="Filtrer">
+      Filtrer
+    </div>
+    <div>
+
+    </div>
   </main>
 </template>
+
+<style>
+.Filtrer{
+
+  
+}
+</style>
