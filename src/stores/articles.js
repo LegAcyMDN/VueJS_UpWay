03b0171aef610e2e0b10a4658d24f99265a2933b
droueest <alexis.bonnard@etu.univ-smb.fr>
2025-03-31 08:51:52 +0000

    Add : add new articles

--- src/stores/articles.js
+++ src/stores/articles.js
@@ -5,9 +5,27 @@ import axios from 'axios'
 export const useArticleStore = defineStore('article', () => {
   const listarticle = ref([])
   const connected = ref(false)
+  const nouvelart = 
+  {
+    titreCategorieArticle: "",
+    contenuCategorieArticle : "",
+    imageCategorie: "",
+  }
+  function ajouter(){
+    axios.post(`${window.VITE_BACKEND_URL}/CategorieArticles`,this.nouvelart).then(response =>
+      {
+        if(response.data.console.error == true)
+          {
+            console.log("ptdr t'es qui")
+          }
+          else
+            {
 
+            }
+      })
+  }
   axios.get(`${window.VITE_BACKEND_URL}/CategorieArticles`).then((response) => {
     listarticle.value = response.data
   })
-  return { listarticle, connected }
+  return { listarticle, connected, nouvelart, ajouter}
 })
