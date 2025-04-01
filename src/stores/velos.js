5ff0679a60e9d1bbe722948ad8dd19e1ec21f265
LegAcyMDN <binanseajustin@gmail.com>
2025-04-01 15:35:05 +0000

    add pagination for velo

--- src/stores/velos.js
+++ src/stores/velos.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 export const useVelosStore = defineStore('velos', () => {
   const list = ref([])
   const cart = ref([])
+  const photos = ref([])
 
     const connected = ref(false)
 
@@ -12,13 +13,12 @@ export const useVelosStore = defineStore('velos', () => {
         .then(response => {
             list.value = response.data
         })
-    async function fetchBikes(page) {
-        this.currentPage = page;
-        response = await axios.get(
-          `${window.VITE_BACKEND_URL}/Velos?=${page}`
-        );
-        list.value = response.data;
-        
+    function fetchBikes(page) {
+        //location.reload();
+        axios.get(`${window.VITE_BACKEND_URL}/Velos?page=${page}`)
+            .then(response => {
+                list.value = response.data
+            })
     }
     function add(velo) {
         return axios.post(`${window.VITE_BACKEND_URL}/Velos`, velo.value)
