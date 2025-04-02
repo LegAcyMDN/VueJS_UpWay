d21893e116dd3966da0eb8ddf58f52fbd8f1a7ad
RedBoxing <redboxing@redboxing.moe>
2025-04-02 08:47:31 +0200

    Fix: velo pages not working correctly

--- src/stores/velos.js
+++ src/stores/velos.js
@@ -6,28 +6,27 @@ export const useVelosStore = defineStore('velos', () => {
   const list = ref([])
   const cart = ref([])
 
-    axios.get(`${window.VITE_BACKEND_URL}/Velos`).then(response => {
-        list.value = response.data
+  axios.get(`${window.VITE_BACKEND_URL}/Velos`).then((response) => {
+    list.value = response.data
+  })
+
+  function add(velo) {
+    return axios.post(`${window.VITE_BACKEND_URL}/Velos`, velo.value)
+  }
+
+  async function getById(id) {
+    return (await axios.get(`${window.VITE_BACKEND_URL}/Velos/GetById/${id}`)).data
+  }
+
+  async function getPhotoById(id) {
+    return (await axios.get(`${window.VITE_BACKEND_URL}/Velos/GetPhotosById/${id}`)).data
+  }
+
+  function fetchBikes(page) {
+    axios.get(`${window.VITE_BACKEND_URL}/Velos?page=${page}`).then((response) => {
+      list.value = response.data
     })
+  }
 
-    function add(velo) {
-        return axios.post(`${window.VITE_BACKEND_URL}/Velos`, velo.value)
-    }
-
-    async function getById(id) {
-      return (await axios.get(`${window.VITE_BACKEND_URL}/Velos/GetById/${id}`)).data
-    }
-
-    async function getPhotoById(id) {
-        return (await axios.get(`${window.VITE_BACKEND_URL}/Velos/GetPhotosById/${id}`));
-    }
-
-    function fetchBikes(page) {
-        axios.get(`${window.VITE_BACKEND_URL}/Velos?page=${page}`)
-            .then(response => {
-                list.value = response.data
-            })
-    }
-
-    return { list, cart, add, getPhotoById, getById, fetchBikes }
+  return { list, cart, add, getPhotoById, getById, fetchBikes }
 })
