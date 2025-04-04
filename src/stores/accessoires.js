8c352d7d5077127b60b2c33ff411c4f492d427de
AlexisBONNARD <Alexis.bonnard@etu.univ-smb.fr>
2025-04-04 07:46:29 +0000

    refactor: assecoire js enlever les deux inutile

--- src/stores/accessoires.js
+++ src/stores/accessoires.js
@@ -42,28 +42,11 @@ export const useAccessoiresStore = defineStore('accesoires', () => {
     return entries
   }
 
-  async function getByPrix(min, max, page = 0) {
-    list.value = (await axios.get(
-      `${window.VITE_BACKEND_URL}/Accessoires/GetByPrix`,
-      {
-        params: { min, max, page }
-      }
-    )).data
-    current_page.value = page
-  }
-
-  async function getByCategory(category, page = 0) {
-    list.value = (await axios.get(
-      `${window.VITE_BACKEND_URL}/Accessoires/GetByCategory/${category}?page=${page}`
-    )).data
-    current_page.value = page
-  }
-
-  async function getByCategoryPrix(category, min, max, page = 0) {
+  async function getByCategoryPrix(categoryId, min, max, page = 0) {
     list.value = (await axios.get(
       `${window.VITE_BACKEND_URL}/Accessoires/GetByCategoryPrix`,
       {
-        params: { category, min, max, page }
+        params: { categoryId, min, max, page }
       }
     )).data
     current_page.value = page
@@ -87,8 +70,6 @@ export const useAccessoiresStore = defineStore('accesoires', () => {
     add,
     getPhotosById,
     getById,
-    getByPrix,
-    getByCategory,
     getByCategoryPrix,
     fetchAccessories,
     count,
