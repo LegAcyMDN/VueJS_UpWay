272e07d9aab1b6fbcc86a258f419ea9498d55299
--local <quentin.dacquin@etu.univ-smb.fr>
2025-04-07 06:44:58 +0000

    refactor : modale, croix and entête

--- src/components/ModaleInspection.vue
+++ src/components/ModaleInspection.vue
@@ -4,7 +4,7 @@
   
       <div class="modale card">
     <div v-on:click="toggleModale" class="btn-modale btn btn-danger">
-
+      <svg width="24" height="24" stroke="#1c1535" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"></path></svg>
     </div>
 
   
@@ -20,7 +20,7 @@
 
       <div>
         <div class="info" id="entête">
-          <p class="critère">Mécanique</p>
+          <p class="critère">{{ estrealises[0].estRealiseRapportInspection.typeInspection }}</p>
           <p>Réparé / remplacé</p>
           <p>Validé</p>
         </div>
